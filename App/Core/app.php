616d8052952631f685f853c2e8c7bb1eafee5556
iamcelestino <trcelestino488@gmail.com>
2025-06-15 14:33:53 -0700

    feat: add home controller

--- App/Core/app.php
+++ App/Core/app.php
@@ -1,21 +1,23 @@
 <?php
 declare(strict_types=1);
 
+namespace App\Core;
+
 class App 
 {
-    protected string|object $controller = 'home';
+    protected string|object $controller = 'HomeController';
     protected string $method            = 'index';
     protected array $params             = [];
 
     public function __construct()
     {
         $URL = $this->parse_url();
-        if(file_exists("../App/Controllers" . ucfirst($URL[0]) . "Controller.php")) {
+        if(file_exists("../App/Controllers" . ucfirst($URL[0]) . " Controller.php")) {
             $this->controller = $URL[0];
             unset($URL[0]);
         }
 
-        $controllerClass = "App\\Controllers" . $this->controller;
+        $controllerClass = "App\\Controllers\\" . $this->controller;
         $this->controller = new $controllerClass();
 
         if(isset($URL[1])) {
@@ -31,7 +33,7 @@ class App
 
     private function parse_url() 
     {
-        $url = isset($_GET['url']) ? $_GET['url'] : 'homeController';
+        $url = isset($_GET['url']) ? $_GET['url'] : 'HomeController';
         return explode("/", filter_var(trim($url, "/"), FILTER_SANITIZE_URL));
     }
 }
---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require __DIR__ . '/../App/Core/config.php';
+require __DIR__ . '/../vendor/autoload.php';
+
+use App\Core\App;
+
+$app = new App();
